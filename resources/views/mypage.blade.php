5151b7a576bcd2a89cfec76d01e94f0bd5e9be80
Shahbaz Quadri <sbzquadri@gmail.com>
2020-01-12 21:04:43 +0530

    laravel project

---
+++ resources/views/mypage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Vashu</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, eaque. Aliquid quia repudiandae porro accusantium ex, nemo voluptate expedita exercitationem rerum veritatis fugiat impedit eaque consectetur harum dolorum vel voluptatibus asperiores molestias illo rem omnis animi temporibus nobis odio. Laborum accusantium vero aliquam amet obcaecati perspiciatis architecto sunt quis sed ad veritatis optio dolore nulla qui rem minima aut, porro dolorum. Voluptates minus dolores quasi voluptate, quae consectetur aperiam. Fugiat ipsum eos provident, debitis quaerat mollitia porro reiciendis obcaecati voluptates? Rem id possimus expedita! Aperiam, ea ipsa in dolorum distinctio delectus tempora porro excepturi veniam voluptates architecto nostrum consequuntur dicta?</p>
+</body>
+</html>
